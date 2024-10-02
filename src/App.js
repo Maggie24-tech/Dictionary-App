00c40afd406290b9001f8b86d736dca9c0395eba
Maggie24-tech <107142910+Maggie24-tech@users.noreply.github.com>
2024-10-02 01:49:57 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,25 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <footer className="App-footer">
-          <small>Coded by Magdalene Nsiah</small>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Maggie24-tech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Magdalene Nsiah
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Maggie24-tech/React-Dictionary-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://reacweatherapp.netlify.app"> Netlify </a>
         </footer>
       </div>
     </div>
